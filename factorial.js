b5ef8e3a085492b91a00ba010d2949abc24711ca
Trent Smith <trentnsmith@gmail.com>
2020-09-02 21:02:56 -0500

    .

--- factorial.js
+++ factorial.js
@@ -7,3 +7,20 @@ function factorial(n){
   }
   
   factorial(5)
+
+  function findMin(array){
+    let min = array[0], ticks = 1;
+    for (let i = 1; i < array.length; i++) {
+        ticks++;
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return {
+        result: min,
+        ticks: ticks
+    };
+}
+
+findMin([1,2,3]);
+findMin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
--- nthTriNumber.js
+++ nthTriNumber.js
@@ -10,3 +10,31 @@ function nthTriNum(x, y) {
   }
   
   console.log(nthTriNum(5, 3));
+
+
+
+  function countTriangle(layers) {
+    let ticks = 1;
+    let count = 0; // the number of dots we've counted so far
+    let layer = 0; // the current layer we're on
+    let lastLayer = 1; // the number of dots we counted in the previous layer
+    while (layer < layers) {
+        ticks++;
+        let newLayer = 0; // the number of dots we've counted so far in the current layer
+        for (let i = 0; i < lastLayer; i++) {
+            ticks++;
+            newLayer += 2;
+        }
+        lastLayer = newLayer;
+        count += lastLayer;
+        layer++;
+    }
+    return {
+        result: count,
+        ticks: ticks
+    };
+}
+
+countTriangle(2);
+countTriangle(4);
+countTriangle(16);
